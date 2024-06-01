35d60f0c143c9df16db02650b1170be30921c2c2
“Cooper Brady” <cbra207@wgu.edu>
2024-06-01 17:07:11 -0600

    updating for cool pics 2

---
+++ week3/coolpics/events.js
@@ -0,0 +1,131 @@
+// events.js
+let tasks = [];
+
+function todoTemplate(task) {
+    return `<li ${task.completed ? 'class="strike"' : ""}>
+    <p>${task.detail}</p>
+    <div>
+      <span data-function="delete">❎</span>
+      <span data-function="complete">✅</span>
+    </div>
+  </li>`;
+}
+function renderTasks(tasks) {
+  // get the list element from the DOM
+  // loop through the tasks array. transform (map) each task object into the appropriate HTML to represent a to-do.
+  console.log("render", tasks);
+  const html = tasks.map(todoTemplate);
+  console.log(html);
+  const listElement = document.querySelector('#todoList');
+  listElement.innerHTML = html.join('');
+}
+
+
+function newTask() {
+  // get the value entered into the #todo input
+  const todoInput = document.querySelector("#todo");
+  console.log(todoInput.value);
+  const newTask = {
+    detail: todoInput.value,
+    completed: false
+  }
+  // add it to our arrays tasks
+  tasks.push(newTask);
+  // render out the list
+  renderTasks(tasks);
+}
+
+function removeTask(taskElement) {
+  // Note the use of Array.filter to remove the element from our task array
+  // Notice also how we are using taskElement instead of document as our starting point?
+  // This will restrict our search to the element instead of searching the whole document.
+  tasks = tasks.filter(
+    (task) => task.detail != taskElement.querySelector('p').innerText
+  );
+
+  // this line removes the HTML element from the DOM
+  taskElement.remove();
+}
+
+function completeTask(taskElement) {
+  // In this case we need to find the index of the task so we can modify it.
+  const taskIndex = tasks.findIndex(
+    (task) => task.detail === taskElement.childNodes[0].innerText
+  );
+  // once we have the index we can modify the complete field.
+  // tasks[taskIndex].completed ? false : true is a ternary expression.
+  // If the first part is true (left of the ?), then the value on the left of the : will get returned, otherwise the value on the right of the : will be returned.
+  tasks[taskIndex].completed = tasks[taskIndex].completed ? false : true;
+  // toggle adds a class if it is not there, removes it if it is.
+  taskElement.classList.toggle("strike");
+  console.log(tasks);
+}
+
+function manageTasks(event) {
+  // did they click the delete or complete icon?
+  console.log(event.target);
+  console.log(event.currentTarget);
+  // event.target will point to the actual icon clicked on. We need to get the parent li to work with however. HINT: Remember element.closest()? Look it up if you don't
+
+  // because we added 'data-action="delete"' to each icon in a task we can access a dataset property on our target (e.target.dataset.action)
+  // use that in a couple of if statements to decide whether to run removeTask or completeTask
+}
+
+// Add your event listeners here
+// We need to attach listeners to the submit button and the list. Listen for a click, call the 'newTask' function on submit and call the 'manageTasks' function if either of the icons are clicked in the list of tasks.
+document.querySelector("#submitTask").addEventListener("click", newTask);
+
+
+
+
+
+
+// coolpics.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuButton = document.querySelector(".menu-button");
+  const menuItems = document.querySelectorAll(".menu-item");
+
+  function toggleMenu() {
+      menuItems.forEach(item => item.classList.toggle("hide"));
+  }
+
+  function handleResize() {
+      if (window.innerWidth > 1000) {
+          menuItems.forEach(item => item.classList.remove("hide"));
+      } else {
+          menuItems.forEach(item => item.classList.add("hide"));
+      }
+  }
+
+  menuButton.addEventListener("click", toggleMenu);
+  window.addEventListener("resize", handleResize);
+  handleResize(); // Call on initial load
+
+  document.querySelector(".gallery").addEventListener("click", viewHandler);
+});
+
+function viewerTemplate(src, alt) {
+  return `
+      <div class="viewer">
+          <button class="close-viewer">X</button>
+          <img src="${src}" alt="${alt}">
+      </div>
+  `;
+}
+
+function viewHandler(event) {
+  const target = event.target;
+  if (target.tagName === 'IMG') {
+      const src = target.src;
+      const parts = src.split("-");
+      const newSrc = `${parts[0]}-full.jpeg`;
+      const alt = target.alt;
+      document.body.insertAdjacentHTML("afterbegin", viewerTemplate(newSrc, alt));
+      document.querySelector(".close-viewer").addEventListener("click", closeViewer);
+  }
+}
+
+function closeViewer() {
+  document.querySelector(".viewer").remove();
+}
