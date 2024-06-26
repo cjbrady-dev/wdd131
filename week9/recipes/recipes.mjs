1f6711242b0bf72f7e1485b57ad0f68edb3056c2
“Cooper Brady” <cbra207@wgu.edu>
2024-06-26 13:05:54 -0600

    update

---
+++ week9/recipes/recipes.mjs
@@ -0,0 +1,40 @@
+const recipes = [
+    {
+        author: 'Provo High Culinary Students',
+        url: '',
+        isBasedOn: '',
+        cookTime: '30 Min',
+        datePublished: '2016-10-16',
+        tags: ['Waffles', 'Sweet Potato', 'Side'],
+        description: 'Savory waffles made with Sweet potato with a hint of Ginger',
+        image: './images/sweet-potato-waffle.jpg',
+        recipeIngredient: [
+            '2 separated eggs',
+            '1/4 C Oil',
+            '1/4 tsp salt',
+            '1/4 tsp Cayenne pepper',
+            '3/4 C milk',
+            '1 Tbsp Brown Sugar',
+            '2 tsp Shredded Ginger',
+            '3/4 C Mashed Sweet Potatoes',
+            '1 Tbsp Minced Shallots',
+            '1 Tbsp Baking Powder',
+            '1 Tbsp Chives',
+            '1/4 C Cornmeal',
+            '1 C Flour'
+        ],
+        name: 'Sweet Potato Waffles',
+        prepTime: '30 Min',
+        recipeInstructions: [
+            'Add the egg yolks, oil, salt, cayenne, sugar, ginger, shallots, sweet potatoes (steam and mash before), and milk and mix well.',
+            'Next add the cornmeal, chives, and flour and baking powder',
+            'Whip the egg whites until stiff and fold in',
+            'Cook until golden brown in a waffle iron. Serve with butter and Wilted Greens or Honey.'
+        ],
+        recipeYield: '6 waffles',
+        rating: 4
+    },
+    // Other recipes...
+];
+
+export default recipes;
